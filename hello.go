e753bcb4c8b2a8a59f9726a95098c418e8641f99
Yan Fonseca <52707540+yanfonseca@users.noreply.github.com>
2020-12-12 02:24:11 -0300

    Create hello.go

---
+++ hello.go
@@ -0,0 +1,10 @@
+//mostra que é o programa principal e vai ser executavel
+package main
+
+import "fmt"
+
+//precisa chamar main(), funcao principal
+func main() {
+	fmt.Println("Hello, World!")
+}
+
